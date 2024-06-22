af5a92a5e04d05dce34e71a7c85aa97d51c474d6
diy0r <sabirovdiyorkind@gmail.com>
2024-06-23 01:07:41 +0500

    test: set up jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+};
