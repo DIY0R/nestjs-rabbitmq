5b6d2f2653b8c36d3971d7565dbc8a25d735c325
diy0r <sabirovdiyorkind@gmail.com>
2024-07-03 18:44:22 +0500

    refactor: exact status message

--- lib/rmq.service.ts
+++ lib/rmq.service.ts
@@ -10,18 +10,23 @@ import {
   IMessageBroker,
   INotifyReply,
   IPublishOptions,
+  TypeChanel,
   TypeQueue,
 } from './interfaces';
 import { IMetaTegsMap } from './interfaces/metategs';
 import {
   DEFAULT_TIMEOUT,
+  EMPTY_MESSAGE,
   ERROR_NO_ROUTE,
   INDICATE_REPLY_QUEUE,
   INITIALIZATION_STEP_DELAY,
   INOF_NOT_FULL_OPTIONS,
+  MESSAGE_NON,
   MODULE_TOKEN,
+  NACKED,
   NON_ROUTE,
   RECIVED_MESSAGE_ERROR,
+  RETURN_NOTHING,
   RMQ_APP_OPTIONS,
   RMQ_BROKER_OPTIONS,
   RMQ_MESSAGE_META_TEG,
@@ -71,19 +76,29 @@ export class RmqService implements OnModuleInit, OnModuleDestroy {
     topic: string,
     message: IMessage,
     options?: Options.Publish,
-  ): INotifyReply {
+  ): Promise<INotifyReply> {
     this.initializationCheck();
-    this.rmqNestjsConnectService.publish({
-      exchange: this.options.exchange.exchange,
-      routingKey: topic,
-      content: message,
-      options: {
-        appId: this.options.serviceName,
-        timestamp: new Date().getTime(),
-        ...options,
-      },
+    return new Promise((resolve, reject) => {
+      const confirmationFunction = (err: any, ok: Replies.Empty) => {
+        if (err !== null) return reject(NACKED);
+        resolve({ status: 'ok' });
+      };
+      this.rmqNestjsConnectService.publish(
+        {
+          exchange: this.options.exchange.exchange,
+          routingKey: topic,
+          content: message,
+          options: {
+            appId: this.options.serviceName,
+            timestamp: new Date().getTime(),
+            ...options,
+          },
+        },
+        confirmationFunction,
+      );
+      if (this.globalOptions.typeChanel == TypeChanel.CHANEL)
+        resolve({ status: 'ok' });
     });
-    return { status: 'ok' };
   }
   private async init() {
     this.exchange = await this.rmqNestjsConnectService.assertExchange(
@@ -102,8 +117,11 @@ export class RmqService implements OnModuleInit, OnModuleDestroy {
       this.logger.error(INDICATE_REPLY_QUEUE);
       throw new Error(INDICATE_REPLY_QUEUE);
     }
-
     return new Promise<IReply>(async (resolve, reject) => {
+      if (!message) {
+        this.logger.error(MESSAGE_NON);
+        throw new Error(MESSAGE_NON);
+      }
       const correlationId = getUniqId();
       const timeout =
         options?.timeout ?? this.options.messageTimeout ?? DEFAULT_TIMEOUT;
@@ -114,48 +132,46 @@ export class RmqService implements OnModuleInit, OnModuleDestroy {
         });
         reject(new Error(TIMEOUT_ERROR));
       }, timeout);
-
-      this.sendResponseEmitter.once(correlationId, (msg: Message) => {
-        clearTimeout(timerId);
-        if (msg.properties?.headers?.['-x-error']) {
-          this.logger.error('Received message with error header', {
-            correlationId,
-          });
-          return reject(new Error(RECIVED_MESSAGE_ERROR));
-        }
-
-        try {
-          const { content } = msg;
+      try {
+        this.sendResponseEmitter.once(correlationId, (msg: Message) => {
+          clearTimeout(timerId);
+          if (msg.properties?.headers?.['-x-error']) {
+            this.logger.error('Received message with error header', {
+              correlationId,
+            });
+            return reject(new Error(RECIVED_MESSAGE_ERROR));
+          }
+          const content = msg.content;
           if (content.toString()) {
             resolve(JSON.parse(content.toString()));
           } else {
-            this.logger.error('Received empty message content', {
+            this.logger.error(EMPTY_MESSAGE, {
               correlationId,
             });
-            reject(new Error('Received empty message content'));
+            reject(new Error(EMPTY_MESSAGE));
           }
-        } catch (error) {
-          this.logger.error('Error parsing message content', {
-            correlationId,
-            error,
-          });
-          reject(error);
-        }
-      });
-
-      try {
-        this.rmqNestjsConnectService.publish({
-          exchange: this.options.exchange.exchange,
-          routingKey: topic,
-          content: message,
-          options: {
-            replyTo: this.replyToQueue.queue,
-            appId: this.options.serviceName,
-            correlationId,
-            timestamp: new Date().getTime(),
-            ...options,
-          },
         });
+        const confirmationFunction = (err: any, ok: Replies.Empty) => {
+          if (err) {
+            clearTimeout(timerId);
+            reject(NACKED);
+          }
+        };
+        this.rmqNestjsConnectService.publish(
+          {
+            exchange: this.options.exchange.exchange,
+            routingKey: topic,
+            content: message,
+            options: {
+              replyTo: this.replyToQueue.queue,
+              appId: this.options.serviceName,
+              correlationId,
+              timestamp: new Date().getTime(),
+              ...options,
+            },
+          },
+          confirmationFunction,
+        );
       } catch (error) {
         clearTimeout(timerId);
         this.logger.error('Error publishing message', { correlationId, error });
@@ -165,21 +181,30 @@ export class RmqService implements OnModuleInit, OnModuleDestroy {
   }
 
   private async listenQueue(message: ConsumeMessage | null): Promise<void> {
-    const route = this.getRouteByTopic(message.fields.routingKey);
-    const consumeFunction =
-      this.rmqMessageTegs.get(route) || this.rmqMessageTegs.get(NON_ROUTE);
-    let result = { error: ERROR_NO_ROUTE };
-    if (consumeFunction)
-      result = await consumeFunction(
-        JSON.parse(message.content.toString()),
-        message,
-      );
-    if (message.properties.replyTo) {
-      await this.rmqNestjsConnectService.sendToReplyQueue({
-        replyTo: message.properties.replyTo,
-        content: result || { status: 'recived' },
-        correlationId: message.properties.correlationId,
-      });
+    try {
+      const messageParse = JSON.parse(message.content.toString());
+      if (!message) throw new Error('Received null message');
+      const route = this.getRouteByTopic(message.fields.routingKey);
+      const consumeFunction =
+        this.rmqMessageTegs.get(route) || this.rmqMessageTegs.get(NON_ROUTE);
+
+      let result = { error: ERROR_NO_ROUTE };
+      if (consumeFunction) {
+        result = (await consumeFunction(messageParse, message)) || {
+          info: RETURN_NOTHING,
+        };
+      }
+
+      if (message.properties.replyTo) {
+        await this.rmqNestjsConnectService.sendToReplyQueue({
+          replyTo: message.properties.replyTo,
+          content: result,
+          correlationId: message.properties.correlationId,
+        });
+      }
+    } catch (error) {
+      this.logger.error('Error processing message', { error, message });
+      this.rmqNestjsConnectService.nack(message, false, false);
     }
   }
 
@@ -232,6 +257,11 @@ export class RmqService implements OnModuleInit, OnModuleDestroy {
     ...params: Parameters<Channel['ack']>
   ): ReturnType<Channel['ack']> {
     return this.rmqNestjsConnectService.ack(...params);
+  }
+  public nack(
+    ...params: Parameters<Channel['nack']>
+  ): ReturnType<Channel['nack']> {
+    return this.rmqNestjsConnectService.nack(...params);
   }
   private async initializationCheck() {
     if (this.isInitialized) return;
