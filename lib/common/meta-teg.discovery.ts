812a0292b3259ade9a25935ee783e3c98ef972a8
diy0r <sabirovdiyorkind@gmail.com>
2024-07-10 17:54:22 +0500

    fix: skip check non objects

--- lib/common/meta-teg.discovery.ts
+++ lib/common/meta-teg.discovery.ts
@@ -1,5 +1,5 @@
-import { Injectable, Type } from '@nestjs/common';
-import { ModulesContainer, Reflector } from '@nestjs/core';
+import { Injectable } from '@nestjs/common';
+import { ModuleRef, ModulesContainer, Reflector } from '@nestjs/core';
 import { MetadataScanner } from '@nestjs/core';
 import { InstanceWrapper } from '@nestjs/core/injector/instance-wrapper';
 import { MESSAGE_ROUTER, MODULE_TOKEN, SER_DAS_KEY } from '../constants';
@@ -16,15 +16,14 @@ export class MetaTegsScannerService {
     private readonly modulesContainer: ModulesContainer,
   ) {}
   public findModulesByProviderValue(tokenValue: string): Module {
-    for (const module of this.modulesContainer.values()) {
-      const importsModules: Module[] = [...module.imports.values()];
-      for (const importedModule of importsModules) {
+    for (const module of this.modulesContainer.values())
+      for (const importedModule of module.imports.values()) {
         const provider = importedModule.providers.get(MODULE_TOKEN);
         if (provider && provider.instance === tokenValue) return module;
       }
-    }
     return null;
   }
+
   public scan(metaTeg: string, tokenValue: string) {
     const rmqMessagesMap: IMetaTegsMap = new Map();
     const currentModule = this.findModulesByProviderValue(tokenValue);
@@ -34,13 +33,14 @@ export class MetaTegsScannerService {
 
     providersAndControllers.forEach((provider: InstanceWrapper) => {
       const { instance } = provider;
-      this.metadataScanner
-        .getAllMethodNames(instance)
-        .forEach((name: string) =>
+      if (instance instanceof Object) {
+        const allMethodNames = this.metadataScanner.getAllMethodNames(instance);
+
+        allMethodNames.forEach((name: string) =>
           this.lookupMethods(metaTeg, rmqMessagesMap, instance, name),
         );
+      }
     });
-
     return rmqMessagesMap;
   }
 
